5c0c75467ccad6062c7c40fb9fdbd62cbc17ffc0
Anastasia Novelly <anastasianovelly@example.com>
2026-07-14 00:13:29 -0500

    fixing site

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from "react-router";
 import ReactGA from "react-ga4";
 import AppRouter from "./router";
 
-ReactGA.initialize("G-BMTYE03CNZ");
+ReactGA.initialize("G-7CVPZRVJQ5");
 
 export default function App() {
   return (
